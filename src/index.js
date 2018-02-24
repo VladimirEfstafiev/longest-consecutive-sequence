cfd82a826b5ccf59284ea6b284db9c9b40f81b7f
Alex Brov <shlak10@outlook.com>
2018-02-24 13:08:31 +0300

    result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+module.exports =function longestConsecutiveLength(nums) {
+    let numsObj={},longestCL=0;
+    for(let i in nums){
+        numsObj[nums[i]]="";
+
+    }
+    for(let key in numsObj){
+        if(!numsObj.hasOwnProperty(key-1)){
+            let length=1,k=key;
+            while (numsObj.hasOwnProperty(+k+1)){
+                k++;
+                length++;
+            }
+            longestCL=longestCL>length?longestCL:length;
+        }
+
+    }
+    return longestCL;
 }
